8a3017af65a825b98b285a7889a2bea10c7fcf87
serge.muradian@gmail.com <serge.muradian@gmail.com>
2023-02-12 13:12:09 +0300

    Add App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React from 'react';
+import { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+// 930634a9
+
+const API_URL = 'http://www.omdbapi.com?apikey=930634a9';
+
+const App = () => {
+
+    const searchMovies = async(title) => {
+        const response = await fetch(`${API_URL}&s=$(title)`);
+        const data = await response.json();
+    }
+
+    useEffect(() => {
+        searchMovies()
+    }, [])
+
+    return (
+        <h1>App</h1>
+    )
 }
 
-export default App;
+export default App
